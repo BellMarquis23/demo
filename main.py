b8ece0abefc4909b421791f2970567948df0d546
Marcus Ponce <BellMarquis23@users.noreply.github.com>
2018-11-09 12:04:10 -0800

    second push

--- main.py
+++ main.py
@@ -1,51 +1,69 @@
 #this file was created by Marcus Ponce
 #thanks Chris Bradfield from Kids Can Code
+#Ive now got this on github
 
+# Sources: goo.gl/2KMivS 
+# now available in github
 import pygame as pg
 import random
 from settings import *
-from sprites import * 
+from sprites import *
 
 class Game:
     def __init__(self):
-        #init game window, try:
+        #init game window
+        # init pygame and create window
         pg.init()
+        # init sound mixer
         pg.mixer.init()
-        self.screen = pg.displau.set_mode(WIDTH, HEIGHT)
-        pg.display.set_caption 
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("jump&shootman")
         self.clock = pg.time.Clock()
-        self.running = True
-        
-        #init pygame and create
+        self.running = True 
     def new(self):
+        # add all sprites to the pg group
         self.all_sprites = pg.sprite.Group()
+        # add a player 1 to the group
         self.player = Player()
         self.all_sprites.add(self.player)
-        #create new player object
+        # add a player 2 to the group
+        self.enemy = Enemy()
+        self.all_sprites.add(self.enemy)
+        # add a platform
+        self.platform = Platform()
+        self.all_sprites.add(self.platform)
+        # call the run method
+        self.run()
     def run(self):
-        
-        #game loop
+        # set boolean playing to true
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
     def update(self):
-        
-        #updates things
+        self.all_sprites.update()
+        print(pg.sprite.groupcollide(self.all_sprites, self.all_sprites, False, False))
     def events(self):
-        pass
-        #listening for events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
     def draw(self):
-        pass
-        #
+        self.screen.fill(REDDISH)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
     def show_start_screen(self):
         pass
-        #
-    def show_go_screen(self): 
+    def show_go_screen(self):
         pass
-        #
-    
-game = Game()
-g.show_start_screen()
+
+g = Game()
+
+g.show_go_screen()
 
 while g.running:
     g.new()
     g.show_go_screen()
-
-pg.quit()
--- settings.py
+++ settings.py
@@ -8,5 +8,5 @@ HEIGHT = 600
 FPS = 30
 # colors
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-REDDISH = (240, 55, 66)
+BLACK = (0,0,0)
+REDDISH = (240,55,66)
